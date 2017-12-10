14e19ca689d71d49a82fe723d44d6239838eb454
sugahara <sugahara@weddingpark.co.jp>
2017-12-10 15:03:21 +0900

    first commit

---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div>
+        <p>{{ message.message }}</p>
+        <small>{{ message.user.name }}</small>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['message'] // ChatLogのv-bind:messageでプロパティを受け取る
+    }
+</script>
+
+<style lang="css">
+    .chat-message {
+        padding: 1rem;
+    }
+
+    .chat-message > p {
+        mergin-bottom: .5rem;
+    }
+</style>
